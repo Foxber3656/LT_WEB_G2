e4cc24b71891e6b7b0c4d9d2626c2151d3231dda
dodinhchien1511-sketch <dodinhchien1511@gmail.com>
2026-06-28 13:25:44 +0700

    Create home.php

---
+++ Outfit Builder + Authentication/pages/home.php
@@ -0,0 +1,12 @@
+<?php
+session_start();
+include "../includes/header.php";
+include "../config/database.php";
+?>
+
+<h1>THE FOX SHOP</h1>
+<p>Build your outfit like a pro</p>
+
+<div class="hero">
+    <h2>🔥 New Streetwear Collection</h2>
+</div>
